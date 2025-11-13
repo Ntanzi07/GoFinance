5529656edfabb565d7decbf09366ddb08183ad50
Ntanzi07 <nathan.tanzi12345@gmail.com>
2025-11-13 11:12:33 -0300

    changing the user paths

--- internal/routes/routes_user.go
+++ internal/routes/routes_user.go
@@ -13,9 +13,13 @@ func setupRoutesUser(app *fiber.App, db *sql.DB) {
 	repo := repository.NewUsersRepository(db)
 	handler := handlers.NewUsersHandler(repo)
 
-	app.Get("/users", handler.GetAllUserHandler)
-	app.Get("/users/:id", handler.GetUserByIdHandler)
-	app.Get("/:name", handler.GetUserByNameHandler)
-	app.Post("/users", handler.CreateUserHandler)
-	app.Delete("/users/:id", handler.DeleteUserHandler)
+	app.Post("/login", handler.LoginUserHandler)
+
+	/*
+		app.Get("/users", handler.GetAllUserHandler)
+		app.Get("/users/:id", handler.GetUserByIdHandler)
+		app.Get("/:name", handler.GetUserByNameHandler)
+		app.Post("/users", handler.CreateUserHandler)
+		app.Delete("/users/:id", handler.DeleteUserHandler)
+	*/
 }
